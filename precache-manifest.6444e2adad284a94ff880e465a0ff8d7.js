706218a5619f775b2ca702d633dd5db2d1579196
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-11-07 10:11:01 +0530

    Updates

---
+++ precache-manifest.6444e2adad284a94ff880e465a0ff8d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058efb8089dc848f967c68b2f124fb52",
+    "url": "/monsters-rolodex-redux/index.html"
+  },
+  {
+    "revision": "af9051035cc76e1e3676",
+    "url": "/monsters-rolodex-redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4bbf7ce2fd6af3ad723f",
+    "url": "/monsters-rolodex-redux/static/css/main.51b12720.chunk.css"
+  },
+  {
+    "revision": "af9051035cc76e1e3676",
+    "url": "/monsters-rolodex-redux/static/js/2.613c7f4f.chunk.js"
+  },
+  {
+    "revision": "4bbf7ce2fd6af3ad723f",
+    "url": "/monsters-rolodex-redux/static/js/main.486472e5.chunk.js"
+  },
+  {
+    "revision": "f5b85e25a3267ff5a4c2",
+    "url": "/monsters-rolodex-redux/static/js/runtime-main.ada21768.js"
+  }
+]);
